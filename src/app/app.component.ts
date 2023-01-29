fd754e2a1bc452fd6d8fe120dbd2abe4215d316f
Derrick Kyereh <69907881+kyerehdi@users.noreply.github.com>
2023-01-29 00:38:21 -0500

    done v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import emailJs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { FormBuilder, FormGroup } from '@angular/forms';
 //import "@fontsource/space-grotesk";
 
 @Component({
@@ -6,7 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'port-app';
   picOfMe = '../assets/mePic1.png';
   blockChain = '../assets/block.jpg';
@@ -16,4 +18,44 @@ export class AppComponent {
   vicWeb = '../assets/vic-web-port.PNG';
   linkden = '../assets/link.png';
   github = '../assets/G.png';
+  email_form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.email_form = this.fb.group({
+      from_name: [''],
+      email_id: [''],
+      subject: [''],
+      message: [''],
+    });
+  }
+  
+  sendEmail(data: Event) {
+    emailJs
+      .sendForm(
+        'service_pipskxg',
+        'template_1x8qdqf',
+        data.target as HTMLFormElement,
+        'ZpnudcA0fXGAF2ERJ'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          if(result.status == 200){
+          alert('Email sent successfully');
+
+          setTimeout(() => {
+            this.ngOnInit();
+          }, 500);
+        }
+        },
+        (error) => {
+          alert('Error sending email, please try again later');
+        }
+      );
+  }
+  cancle() {
+    this.ngOnInit();
+  }
+
 }
