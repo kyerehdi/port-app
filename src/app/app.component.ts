9f0f7a4b9756d0793f17153eccd5215596817036
Derrick Kyereh <69907881+kyerehdi@users.noreply.github.com>
2023-01-28 19:58:18 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import "@fontsource/space-grotesk";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'port-app';
+  picOfMe = '../assets/mePic1.png';
+  blockChain = '../assets/block.jpg';
+  linux ='../assets/linux.png';
+  database='../assets/database-soccer.PNG';
+  race = '../assets/race.PNG';
+  vicWeb = '../assets/vic-web-port.PNG';
+  linkden = '../assets/link.png';
+  github = '../assets/G.png';
 }
